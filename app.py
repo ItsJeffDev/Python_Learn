a387224eb51886dcd93a68cd54877906e2a7cf39
Jeff Kolin <jeffkolinmiranda14@gmail.com>
2025-06-26 18:58:56 +0800

    Simple Guess the Number Game

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-def count_words(sentence):
-    # Haluin ang sentence sa mga salita gamit ang .split() method
-    words = sentence.split()
-    # Count ng bilang ng mga salita
-    word_count = len(words)
-    return word_count
+#create a Guess the Number Game
 
-def main():
-    print("Word Counter Program")
-    print("Magbigay ng isang sentence o paragraph:")
-    # Mag-`input` ng sentence mula sa user
-    sentence = input()
-    # Tumawag sa function para sa counting ng mga salita
-    count = count_words(sentence)
-    # Ipakita ang resulta
-    print(f"Ang bilang ng mga salita ay: {count}")
+import random
+start = True
 
-if __name__ == "__main__":
-    main()
+while(start):
+    rnd = random.randint(1, 10)
+    print("Guess the Number Game", end="")
+    user = int(input("Enter Number: "))
+    if user > rnd:
+        print("Greater Than")
+        print("Restart")
+    elif user < rnd:
+        print("Less Than")
+        print("Restart")
+    elif user == rnd:
+        print("Correct")
+        print("Restart")
+        break
+    else:
+        print("Something Went Wrong")
+        print("Restart")
